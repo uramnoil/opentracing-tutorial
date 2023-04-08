75f272a231920b1e1c3586dc0f113b7f9f1c8b7d
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2023-04-09 02:42:39 +0900

    :sparkles: Javaのlesson 2を修了

--- java/src/main/java/lesson02/exercise/Hello.java
+++ java/src/main/java/lesson02/exercise/Hello.java
@@ -1,4 +1,71 @@
 package lesson02.exercise;
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.Scope;
+import io.opentracing.util.GlobalTracer;
+import io.jaegertracing.Configuration;
+import io.jaegertracing.Configuration.ReporterConfiguration;
+import io.jaegertracing.Configuration.SamplerConfiguration;
+import io.jaegertracing.internal.JaegerTracer;
+import com.google.common.collect.ImmutableMap;
+
 public class Hello {
+
+    private final Tracer tracer;
+
+    private Hello(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    private void sayHello(String helloTo) {
+        Span span = tracer.buildSpan("say-hello").start();
+        try (Scope scope = tracer.scopeManager().activate(span)) {
+            span.setTag("hello-to", helloTo);
+
+            // this goes inside the sayHello method
+            String helloStr = formatString(helloTo);
+            printHello(helloStr);
+        } finally {
+            span.finish();
+        }
+    }
+
+    private String formatString(String helloTo) {
+        Span span = tracer.buildSpan("formatString").start();
+        try (Scope scope = tracer.scopeManager().activate(span)) {
+            String helloStr = String.format("Hello, %s!", helloTo);
+            span.log(ImmutableMap.of("event", "string-format", "value", helloStr));
+            return helloStr;
+        } finally {
+            span.finish();
+        }
+    }
+
+    private void printHello(String helloStr) {
+        Span span = tracer.buildSpan("printHello").start();
+        try (Scope scope = tracer.scopeManager().activate(span)) {
+            System.out.println(helloStr);
+            span.log(ImmutableMap.of("event", "println"));
+        } finally {
+            span.finish();
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Expecting one argument");
+        }
+        String helloTo = args[0];
+        
+        var tracer = initTracer("hello-world");
+        new Hello(tracer).sayHello(helloTo);
+    }
+
+    public static JaegerTracer initTracer(String service) {
+        SamplerConfiguration samplerConfig = SamplerConfiguration.fromEnv().withType("const").withParam(1);
+        ReporterConfiguration reporterConfig = ReporterConfiguration.fromEnv().withLogSpans(true);
+        Configuration config = new Configuration(service).withSampler(samplerConfig).withReporter(reporterConfig);
+        return config.getTracer();
+    }
 }
